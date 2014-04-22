0c2f2b3ae40bded1e3086a7707cfe89a3876e6c0
Bryce Baril <bryce@ravenwall.com>
2014-04-22 13:07:26 -0700

    add auth_pass test to auth test

--- test/commands/auth.js
+++ test/commands/auth.js
@@ -2,6 +2,7 @@ var test = require("tape").test;
 
 var util = require("../util");
 var client = util.getDirtyClient(0, 9006, "filefish.redistogo.com");
+var client2 = util.getDirtyClient(0, 9006, "filefish.redistogo.com", {auth_pass: "664b1b6aaf134e1ec281945a8de702a9"});
 var singleStringReply = util.singleStringReply;
 var integerReply = util.integerReply;
 
@@ -10,7 +11,13 @@ test("auth", function (t) {
     client.auth("664b1b6aaf134e1ec281945a8de702a9", singleStringReply(t));
 });
 
-test("cleanup", function (t) {
+test("auth_pass", function (t) {
     t.plan(2);
+    client2.ping(singleStringReply(t, "PONG"));
+});
+
+test("cleanup", function (t) {
+    t.plan(4);
     client.quit(singleStringReply(t));
+    client2.quit(singleStringReply(t));
 });
